bb331affcc01608dffc6b8e9c16e6b5c0f060b58
Avishek <avishekkhatiwada35@gmail.com>
2024-03-23 14:02:11 -0400

    Added codes for admin

--- attendance/models.py
+++ attendance/models.py
@@ -18,13 +18,23 @@ class Student(models.Model):
     emergency_contact_person = models.CharField(max_length=100, default='')
     emergency_contact_phone = models.CharField(max_length=20, default='')
     picture = models.ImageField(upload_to='student_pictures', default='default_picture.jpg')
-    face_encodings = models.JSONField(null=True, blank=True)  # Field to store face encodings
+
+
+class Admin(models.Model):
+    name = models.CharField(max_length=100, default='')
+    dob = models.DateField(default='2000-01-01')
+    admin_id = models.CharField(max_length=100, default='')
+
+    username = models.CharField(max_length=100, default='')
+    password = models.CharField(max_length=100, default='')
+    confirm_password = models.CharField(max_length=100, default='')
+    college_email = models.EmailField(default='')
+    personal_email = models.EmailField(default='')
+    phone = models.CharField(max_length=20, default='')
+    address = models.TextField(default='Unknown')
+    picture = models.ImageField(upload_to='admin_pictures', default='default_picture.jpg')
 
 class Teacher(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # Add other teacher attributes like department, designation, etc.
-
-class Admin(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # Add other admin attributes if needed
 
